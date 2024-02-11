04d19964b4d2f74cd00acc96d597959f15e1c0a9
Paliak <91493239+Paliak@users.noreply.github.com>
2024-02-12 00:36:08 +0100

    FEAT: rework buildDiff.sh
    - Keeps cache inside container instance when running locally
    - Removes Settings.xml to avoid loading temp builds
    - Changes repo reset to use git
    - Works on a repo copy to allow for changes after running tests

--- spec/buildDiff.sh
+++ spec/buildDiff.sh
@@ -1,40 +1,44 @@
 #!/bin/sh
-cd $WORKDIR
 umask 0
-git config --global --add safe.directory /workdir
+
+# If external cache dir has not been defined keep it inside the container
+if [[ -z "$CACHEDIR" ]]
+then
+    mkdir /tmp/cachedir
+    export CACHEDIR="/tmp/cachedir"
+fi
+
+# Copy mounted workdir to allow for changes during test run
+rm -rf /tmp/workdir && mkdir /tmp/workdir && cp -rf $WORKDIR/. /tmp/workdir/ && cd /tmp/workdir
+
+git config --global --add safe.directory /tmp/workdir
 git config --global --add advice.detachedHead false
 headref=$(git rev-parse HEAD)
 devref=$(git rev-parse origin/dev)
 
+rm -rf /tmp/devref && mkdir /tmp/devref
+rm /tmp/workdir/src/Settings.xml
+cat /tmp/workdir/spec/builds.txt | dos2unix | parallel --will-cite --ungroup --pipe -N50 'LINKSBATCH="$(mktemp){#}"; cat > $LINKSBATCH; BUILDLINKS="$LINKSBATCH" BUILDCACHEPREFIX="/tmp/devref" busted --lua=luajit -r generate' && \
+BUILDCACHEPREFIX='/tmp/devref' busted --lua=luajit -r generate && date > "/tmp/devref/$headref" && echo "[+] Build cache computed for $headref (headref)"
+
+
 if [[ ! -f "$CACHEDIR/$devref" ]] # Output of builds outdated or nonexistent
 then
-	rm $CACHEDIR/*.lua
-    rm $CACHEDIR/*.build
-    rm $CACHEDIR/*.xml
-    cp -rf --parents $WORKDIR /tmp && cd /tmp$WORKDIR && \
-    git restore . && git clean -fd && git checkout origin/dev && \
-    cp $WORKDIR/.busted /tmp$WORKDIR/.busted && cp $WORKDIR/src/HeadlessWrapper.lua /tmp$WORKDIR/src/HeadlessWrapper.lua && rm -rf /tmp$WORKDIR/spec/ && cp -r $WORKDIR/spec/ /tmp$WORKDIR/spec/ && \
-    cat $WORKDIR/spec/builds.txt | dos2unix | parallel --will-cite --ungroup --pipe -N50 'LINKSBATCH="$(mktemp){#}"; cat > $LINKSBATCH; BUILDLINKS=${LINKSBATCH} BUILDCACHEPREFIX=${CACHEDIR} busted --lua=luajit -r generate' && \
-    BUILDCACHEPREFIX=${CACHEDIR} busted --lua=luajit -r generate && date > "$CACHEDIR/$devref" && echo "[+] Build cache computed for $devref (devref)" && cd $WORKDIR
+	rm -rf $CACHEDIR/*
+
+    # Keep new changes to tests related files
+    git diff /tmp/workdir/.busted /tmp/workdir/src/HeadlessWrapper.lua /tmp/workdir/spec/ > /tmp/patch && \
+    git reset --hard origin/dev && git apply /tmp/patch && \
+    cat /tmp/workdir/spec/builds.txt | dos2unix | parallel --will-cite --ungroup --pipe -N50 'LINKSBATCH="$(mktemp){#}"; cat > $LINKSBATCH; BUILDLINKS="$LINKSBATCH" BUILDCACHEPREFIX="$CACHEDIR" busted --lua=luajit -r generate' && \
+    BUILDCACHEPREFIX="$CACHEDIR" busted --lua=luajit -r generate && date > "$CACHEDIR/$devref" && echo "[+] Build cache computed for $devref (devref)"
 fi
 
-if [[ -f "$CACHEDIR/$devref" ]] #Make sure cache of dev branch builds exists and matches current dev
-then
-    rm -rf /tmp/*
-    cat $WORKDIR/spec/builds.txt | dos2unix | parallel --will-cite --ungroup --pipe -N50 'LINKSBATCH="$(mktemp){#}"; cat > $LINKSBATCH; BUILDLINKS=${LINKSBATCH} BUILDCACHEPREFIX="/tmp" busted --lua=luajit -r generate' && \
-    BUILDCACHEPREFIX='/tmp' busted --lua=luajit -r generate && date > "/tmp/$headref" && echo "[+] Build cache computed for $headref (headref)"
-fi
-
-if [[ -f "/tmp/$headref" ]] # Make sure generating builds for current HEAD was successful
-then
-    for build in $CACHEDIR/*.build
-    do
-        BASENAME=$(basename "$build")
-        DIFFOUTPUT=$(diff <(xmllint --exc-c14n "$build") <(xmllint --exc-c14n "/tmp/$BASENAME")) || {
-            echo '```diff'
-            echo "## Savefile Diff for $BASENAME"
-            echo $DIFFOUTPUT
-            echo '```'
-        }
-    done
-fi
+for build in $CACHEDIR/*.build
+do
+    BASENAME=$(basename "$build")
+    # Only print the header if there is a diff to display
+    DIFFOUTPUT=$(diff <(xmllint --exc-c14n "$build") <(xmllint --exc-c14n "/tmp/devref/$BASENAME")) || {
+        echo "## Savefile Diff for $BASENAME"
+        echo "\`\`\`diff\n${DIFFOUTPUT}\n\`\`\`"
+    }
+done
